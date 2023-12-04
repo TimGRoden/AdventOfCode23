1e3873b496c6d4d324cef7ff6d0774e73412a422
Timothy Roden <tgr@barton.ac.uk>
2023-12-04 11:59:28 +0000

    Day1 visualisation added.

--- Day4/ScratchGame.cs
+++ Day4/ScratchGame.cs
@@ -21,14 +21,13 @@ namespace Day4
         {
             for (int i=0;i< cards.Length; i++)
             {
-                //Console.WriteLine($"{quantity[i]} of Card {cards[i].ID}: {cards[i].getMatches()} matches.");
-                for (int j = 1; j <= cards[i].getMatches(); j++)
+                for (int j = 0; j < cards[i].getMatches(); j++)
                 {
-                    quantity[i + j] += quantity[i];
+                    quantity[i + j + 1] += quantity[i];
                 }
             }
             return quantity.Sum();
         }
-        public long getScore() => cards.Aggregate(0, (n, s) => n + s.getScore());
+        public long getScore() => cards.Aggregate(0, (n, c) => n + c.getScore());
     }
 }
